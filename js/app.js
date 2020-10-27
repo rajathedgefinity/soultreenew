99007c161515ffeb38a49b146284ad417ec5411c
ajithpyneeds <ajith.hlk@gmail.com>
2020-10-27 13:41:04 +0530

    navbar transition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+// menu-pages active function ###########################################
+function menuFunc(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  // tabcontent = document.getElementsByClassName("tabcontent");
+  // for (i = 0; i < tabcontent.length; i++) {
+  //   tabcontent[i].style.display = "none";
+  // }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks-menu");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active-menu", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active-menu";
+}
+
+
 // Hamburger Menu working ###########################################
 var btn = $('.btn11');
 
@@ -186,6 +209,7 @@ function serActive() {
     act.style.display = 'none';
   }
 }
+
 
 
 
